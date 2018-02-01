3dd88e308c54544cbc85ce37d120328b0aef16af
haruyasu <harukun2002@gmail.com>
2018-01-31 20:37:20 -0800

    mondo db basic api

--- 06_clipboard/search.py
+++ 06_clipboard/search.py
@@ -1,13 +1,34 @@
+# How to use MongoDB Basic api
 from pymongo import MongoClient
 
 server = MongoClient()
 db = server["test"]
 collection = db["users"]
-search = collection.find({"age":20})
 
+# search = collection.find({"age":20, "country":"canada"})
+# print search.count()
 # for i in search:
-#     print i["country"]
+#     print i
 
-print search.count()
+# search = collection.find({"age":{"$lte":25}}) #less than xx
+# search = collection.find({"age":{"$gte":25}, "country":"usa"}) #greater than xx
+# print search.count()
+# for i in search:
+#     print i
 
-19:49
+# search = collection.find({"country":"canada"}, {"first_name":1, "_id":0}).limit(10)
+# for i in search:
+#     print i
+
+# search = collection.find({"$or":[{"age":30}, {"country":"usa"}]})
+# for i in search:
+#     print i
+
+# search = collection.find({"country":{"$in":["canada", "usa"]}}).sort("first_name", -1)
+# for i in search:
+#     print i
+
+search = collection.find_one({"first_name":"Liam", "last_name":"Jackson"})
+search["age"] = 25
+collection.save(search)
+print search
